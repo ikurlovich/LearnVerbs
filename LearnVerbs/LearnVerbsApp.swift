39a554008a3d33e28f8f7ecffcc54efe8300ff86
ikurlovich <ikurlovich@icloud.com>
2023-05-23 23:28:54 +0400

    Initial Commit

---
+++ LearnVerbs/LearnVerbsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnVerbsApp.swift
+//  LearnVerbs
+//
+//  Created by Илья Курлович on 23.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct LearnVerbsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
